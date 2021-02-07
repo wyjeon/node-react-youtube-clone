1b48479c52cdeb06c30bec6f0cc988b84a7f8267
wyjeon <wyjeon.info@gmail.com>
2021-02-07 15:47:22 +0900

    Multer로 노드 서버에 비디오 저장하기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
+import axios from 'axios';
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -36,6 +37,22 @@ function VideoUploadPage() {
     setCategory(e.currentTarget.value);
   };
 
+  const onDrop = files => {
+    let formData = new FormData();
+    const config = {
+      header: { 'content-type': 'multipart/form-data' },
+    };
+    formData.append('file', files[0]);
+
+    axios.post('/api/video/uploadfiles', formData, config).then(response => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert('비디오 업로드를 실패했습니다.');
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -45,7 +62,7 @@ function VideoUploadPage() {
       <Form onSubmit>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           {/* dropzone */}
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={100000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
