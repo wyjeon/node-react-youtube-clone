96add7600c0bbf9bc724603243e015b3bbf9785f
wyjeon <wyjeon.info@gmail.com>
2021-02-08 19:10:47 +0900

    구독 기능 (2)

--- client/src/components/views/VideoDetailPage/Sections/Subscribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -26,11 +26,39 @@ function Subscribe(props) {
     }
   });
 
+  const onSubscribe = () => {
+    let subscribedVariable = {
+      userTo: props.userTo,
+      userFrom: localStorage.getItem('userId'),
+    };
+
+    //이미 구독 중이라면
+    if (Subscribed) {
+      Axios.post('/api/subscribe/unSubscribe', subscribedVariable).then(response => {
+        if (response.data.success) {
+          setSubscribeNumber(SubscribeNumber - 1);
+          setSubscribed(!Subscribed);
+        } else {
+          alert('구독 취소하는데 실패 했습니다.');
+        }
+      });
+    } else {
+      Axios.post('/api/subscribe/subscribe', subscribedVariable).then(response => {
+        if (response.data.success) {
+          setSubscribeNumber(SubscribeNumber + 1);
+          setSubscribed(!Subscribed);
+        } else {
+          alert('구독하는데 실패 했습니다.');
+        }
+      });
+    }
+  };
+
   return (
     <div>
       <button
         style={{
-          backgroundColor: `${Subscribe ? '#CC0000' : '#AAAAAA'}`,
+          backgroundColor: `${Subscribed ? '#AAAAAA' : '#CC0000'}`,
           borderRadius: '4px',
           color: 'white',
           padding: '10px 6px',
@@ -38,6 +66,7 @@ function Subscribe(props) {
           fontSize: '1rem',
           textTransform: 'uppercase',
         }}
+        onClick={onSubscribe}
       >
         {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
       </button>
